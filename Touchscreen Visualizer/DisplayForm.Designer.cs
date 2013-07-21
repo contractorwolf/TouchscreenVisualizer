54a7cba9b04c7099f9ddc89e23b96b62c8568921
contractorwolf <contractorwolf@hotmail.com>
2013-07-21 14:19:22 -0400

    initial commit

---
+++ Touchscreen Visualizer/DisplayForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Touchscreen_Visualizer
+{
+    partial class DisplayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.pbTouchPanel = new System.Windows.Forms.PictureBox();
+            this.pSerialIndicator = new System.Windows.Forms.Panel();
+            this.lStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lTranslated = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTouchPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(652, -1);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(156, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(0, 0);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(293, 38);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(525, -1);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(121, 39);
+            this.cbPorts.TabIndex = 2;
+            // 
+            // pbTouchPanel
+            // 
+            this.pbTouchPanel.BackColor = System.Drawing.Color.Black;
+            this.pbTouchPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbTouchPanel.Location = new System.Drawing.Point(0, 0);
+            this.pbTouchPanel.Name = "pbTouchPanel";
+            this.pbTouchPanel.Size = new System.Drawing.Size(811, 574);
+            this.pbTouchPanel.TabIndex = 3;
+            this.pbTouchPanel.TabStop = false;
+            // 
+            // pSerialIndicator
+            // 
+            this.pSerialIndicator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pSerialIndicator.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pSerialIndicator.Location = new System.Drawing.Point(762, 197);
+            this.pSerialIndicator.Name = "pSerialIndicator";
+            this.pSerialIndicator.Size = new System.Drawing.Size(44, 35);
+            this.pSerialIndicator.TabIndex = 5;
+            // 
+            // lStatus
+            // 
+            this.lStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lStatus.AutoSize = true;
+            this.lStatus.BackColor = System.Drawing.Color.Black;
+            this.lStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lStatus.ForeColor = System.Drawing.Color.White;
+            this.lStatus.Location = new System.Drawing.Point(696, 198);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(41, 13);
+            this.lStatus.TabIndex = 6;
+            this.lStatus.Text = "status";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lTranslated
+            // 
+            this.lTranslated.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lTranslated.AutoSize = true;
+            this.lTranslated.BackColor = System.Drawing.Color.Black;
+            this.lTranslated.ForeColor = System.Drawing.Color.White;
+            this.lTranslated.Location = new System.Drawing.Point(695, 45);
+            this.lTranslated.Name = "lTranslated";
+            this.lTranslated.Size = new System.Drawing.Size(55, 13);
+            this.lTranslated.TabIndex = 7;
+            this.lTranslated.Text = "press start";
+            // 
+            // DisplayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 574);
+            this.Controls.Add(this.lTranslated);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.pSerialIndicator);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.pbTouchPanel);
+            this.Name = "DisplayForm";
+            this.Text = "Touchscreen Visualizer ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTouchPanel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.PictureBox pbTouchPanel;
+        private System.Windows.Forms.Panel pSerialIndicator;
+        private System.Windows.Forms.Label lStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lTranslated;
+    }
+}
+
---
+++ Touchscreen Visualizer/DisplayForm.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace Touchscreen_Visualizer
+{
+    public partial class DisplayForm : Form
+    {
+        //arduino as a serial device at 9600
+        public static SerialPort arduino = null;
+        private int serialSpeed = 9600;
+        private Color BackgroundColor = Color.Black;
+
+        private StringBuilder buffer = new StringBuilder();//incoming serial data        
+        public static byte[] rx_data;//serial data
+
+        //touchscreen hardcoded values, used to convert to more usable values
+        private const int touchscreenMinimumX = 120;
+        private const int touchscreenMinimumY = 170;
+        private const int touchscreenMaxWidth = 920;
+        private const int touchscreenMaxHeight = 860;
+
+        //touchscreen touch values
+        private int x = 0;
+        private int y = 0;
+        private int pressure = 0;
+
+        private Bitmap b;//for drawing dots
+        private Graphics g;//for drawing dots
+
+        public DisplayForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b = new Bitmap(this.Width, this.Height, PixelFormat.Format24bppRgb);
+            g = Graphics.FromImage(b);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            //load visible com ports
+            cbPorts.DataSource = SerialPort.GetPortNames();
+            cbPorts.Update();
+
+            lStatus.Text = "started";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (cbPorts.SelectedValue != null)
+            {
+                //start communication with arduino
+                arduino = new SerialPort(cbPorts.SelectedValue.ToString(), serialSpeed);
+                arduino.Open();
+                arduino.DataReceived += new SerialDataReceivedEventHandler(arduino_DataReceived);
+
+                pSerialIndicator.BackColor = Color.Green;
+
+                //clear screen
+                g.Clear(BackgroundColor);
+                pbTouchPanel.Image = b;
+
+                lStatus.Text = "waiting...";
+                lTranslated.Text = "touch screen";
+            }
+            else
+            {
+                MessageBox.Show("ERROR: No COM Port selected");
+            }
+        }
+
+        private void arduino_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {//serial data comes in from the arduino
+            try
+            {
+                rx_data = new byte[arduino.BytesToRead];
+
+                arduino.Read(rx_data, 0, rx_data.Length);
+
+                //add current serial data to buffer
+                buffer.Append(Encoding.ASCII.GetString(rx_data));
+
+                //message blocks are readings from the arduino
+                string[] messageBlocks = buffer.ToString().Split('*');
+
+                if (messageBlocks.Length>=3)
+                {//wait for 3 readings to ensure that one will be whole
+                    this.Invoke(new EventHandler(WriteData));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void WriteData(object sender, EventArgs e)
+        {
+            //clear prev drawn points
+            g.Clear(BackgroundColor);
+
+            //load and clear buffer
+            string current = buffer.ToString();
+            buffer.Clear();
+
+            //split into message blocks
+            string[] messageBlocks = current.Split('*');
+
+            //get second messageBlock, split into values
+            string[] dataPoints = messageBlocks[1].Split(',');
+
+            if (dataPoints.Length == 3)
+            {
+                //show incoming reading
+                txtMessage.Text = messageBlocks[1];
+
+                //conert string data to int
+                int.TryParse(dataPoints[0], out x);
+                int.TryParse(dataPoints[1], out y);
+                int.TryParse(dataPoints[2], out pressure);
+
+                //conert touchscreen point to something relating to the output sizes
+                Point output  = ConvertTouchscreen(new Point { X = x, Y = y },pressure);
+
+                //convert the reverse pressure value
+                int pressureConverted = (int)(100 - (int)(pressure / 8));
+
+                //draw touchpoint
+                //subtraacking the pressure over 2 corrects for the size of the circle
+                //at the top left point, not needed but means the center of circle is where 
+                //the point actually is, as opposed to the top left of the circle
+                int localX = output.X- ((int)(pressureConverted/2));
+                int localY = output.Y- ((int)(pressureConverted/2));
+
+                g.DrawEllipse(new Pen(Color.White, 20), localX, localY, pressureConverted, pressureConverted);
+
+                //show that data has been received
+                pSerialIndicator.BackColor = Color.Blue;
+                lStatus.Text = "receiving";
+
+                //start countdown to switch the indicator back
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+
+            //load drawn points
+            pbTouchPanel.Image = b;    
+        }
+
+        private Point ConvertTouchscreen(Point touchscreen, int pressure)
+        {//convert the incoming touchscreen data into more sensible values
+            Point output = new Point();
+            StringBuilder debugMessage = new StringBuilder();
+
+            //calculations
+            float dx = touchscreen.X - touchscreenMinimumX;
+            float ratioX = dx / (touchscreenMaxWidth-touchscreenMinimumX);
+
+            float dy = touchscreen.Y - touchscreenMinimumY;
+            float ratioY = dy / (touchscreenMaxHeight-touchscreenMinimumY);
+
+            int pressureConverted = (int)(100 - (int)(pressure / 7));
+
+            output.X = this.Width - (int)(ratioX * this.Width);
+            output.Y =  (int)(ratioY * this.Height);
+
+
+
+            //output all the calculated data, to check the math, can be commented out
+            debugMessage.Append("touchscreenx:");
+            debugMessage.Append(touchscreen.X);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("touchscreeny:");
+            debugMessage.Append(touchscreen.Y);
+            debugMessage.Append(Environment.NewLine);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("pressure:");
+            debugMessage.Append(pressure);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("pressure converted:");
+            debugMessage.Append(pressureConverted);
+            debugMessage.Append(Environment.NewLine);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("ratiox:");
+            debugMessage.Append(ratioX);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("ratioy:");
+            debugMessage.Append(ratioY);
+            debugMessage.Append(Environment.NewLine);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("outputx:");
+            debugMessage.Append(output.X);
+            debugMessage.Append(Environment.NewLine);
+
+            debugMessage.Append("outputy:");
+            debugMessage.Append(output.Y);
+            debugMessage.Append(Environment.NewLine);
+
+            //show calculated values
+            lTranslated.Text = debugMessage.ToString();
+
+            return output;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {//switch the indicator back if more than 200ms have elapsed since last reading
+            pSerialIndicator.BackColor = Color.Green;
+            lStatus.Text = "waiting";
+            timer1.Enabled = false;
+
+            //clear last reading
+            g.Clear(BackgroundColor);
+            pbTouchPanel.Image = b;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {//when form size has changed, must update bitmap and graphics
+            b = new Bitmap(this.Width, this.Height, PixelFormat.Format24bppRgb);
+            g = Graphics.FromImage(b);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+    }
+}
